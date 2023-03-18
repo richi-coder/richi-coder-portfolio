4f15cb14b2ef0e03b43687018c95afa61e2047ec
richi-coder <richicoder@gmail.com>
2023-03-18 04:49:07 -0400

    images added form Figma but snipping

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,9 +2,9 @@ import "./styles/typing.css"
 
 export default function Project() {
     return (
-      <div style={{backgroundColor: "transparent"}}>
-        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-12 lg:px-8">
-          <div style={{boxShadow: ".5vw .5vw 0.5vw white "}} className="relative isolate overflow-hidden bg-cyan-600 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+      <div className='bg-transparent'>
+        <div className="mx-auto max-w-full py-24 sm:px-6 sm:py-12 lg:px-8">
+          <div className="relative isolate overflow-hidden bg-cyan-600 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-1 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
               viewBox="0 0 1024 1024"
               className="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:translate-y-0 lg:-translate-x-1/2"
@@ -18,7 +18,7 @@ export default function Project() {
                 </radialGradient>
               </defs>
             </svg>
-            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-48 lg:text-left">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
                 Richi-Shop
                 <br />
@@ -40,15 +40,20 @@ export default function Project() {
               </div>
             </div>
             <div className="relative mt-16 h-80 lg:mt-8">
-              <div>
               <img
-                className="absolute top-0 -left-1/3 w-[45rem] max-w-none"
-                src="/laptop.png"
+                className="absolute top-0 left-0 w-[24rem] sm:w-[50rem] max-w-none rounded-md"
+                src="/richi-shop-shot.png"
                 alt="App screenshot"
-                width={1149}
+                with={1149}
                 height={681}
               />
-              </div>
+              <img
+                className="absolute top-1/3 left-[14rem] sm:-left-[7rem] w-[7rem] sm:w-[20rem] max-w-none"
+                src="/richi-shop-android.png"
+                alt="App screenshot"
+                with={1149}
+                height={681}
+              />
             </div>
           </div>
         </div>
