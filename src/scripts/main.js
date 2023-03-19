cf91720511c5299b5f6a231238bacb24a22c2c72
richi-coder <richicoder@gmail.com>
2023-03-19 17:12:03 -0400

    creating firstAnimation, WITHOUT JAVASCRIPT

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,5 @@
 import { scrollFunction } from "./scrollFunction";
 
-const body = document.querySelector(".body-overflow");
-body.classList.remove("body-overflow")
-
 window.addEventListener("scroll", () => {
     scrollFunction("richi-shop", "notShown", "showUp")
     scrollFunction("4pics-1word", "notShown", "showUp")
