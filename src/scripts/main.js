75f7df54016850d2c628039cb68033c117a9b4d7
richi-coder <richicoder@gmail.com>
2023-03-19 05:03:19 -0400

    preload improved for firstAnimation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 import { scrollFunction } from "./scrollFunction";
 
+const body = document.querySelector(".body-overflow");
+body.classList.remove("body-overflow")
+
 window.addEventListener("scroll", () => {
     scrollFunction("richi-shop", "notShown", "showUp")
     scrollFunction("4pics-1word", "notShown", "showUp")
