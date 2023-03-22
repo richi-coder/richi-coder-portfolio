4d2b24ad8edf9f4d0e1ac132ef1caa6abc798ab1
richi-coder <richicoder@gmail.com>
2023-03-22 00:14:46 -0400

    Using smooth-scroll, problem appears for firstView, since the usage of this library prevents it to keep fixed, there is a way to troubleshoot this, but it is not working now

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,25 @@
 import { scrollFunction } from "./scrollFunction";
+import Scrollbar from 'smooth-scrollbar';
+let scrollbar = Scrollbar.init(document.body);
 
-window.addEventListener("scroll", () => {
-    scrollFunction("richi-shop", "notShown", "showUp")
-    scrollFunction("4pics-1word", "notShown", "showUp")
-    scrollFunction("calculator", "notShown", "showUp")
+window.addEventListener("DOMContentLoaded", () => {
+  // Video: injecting a class to make it not visible
+  const video = document.getElementById("video");
+  video.classList.add("videoNotShown");
+  // Projects: injecting a class to make them not visible
+  const projects = Array.from(document.querySelectorAll(".project-component"));
+  projects.forEach(project => {
+    console.log(project)
+    project.classList.add("notShown")
+  })
+})
+
+scrollbar.addListener(() => {
+  console.log("Scrolling")
+    scrollFunction("PORTFOLIO", "notShown", "showUp")
+    scrollFunction("RICHI-SHOP", "notShown", "showUp")
+    scrollFunction("4PICS-1WORD", "notShown", "showUp")
+    scrollFunction("CALCULATOR", "notShown", "showUp")
     scrollFunction("video", "videoNotShown", "showVideo")
     scrollFunction("projects-title", "titleNotShown", "showTitle")
   })
