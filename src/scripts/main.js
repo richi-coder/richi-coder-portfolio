413774af00b181982bc3359425954b48a6533b21
richi-coder <richicoder@gmail.com>
2023-03-23 01:13:17 -0400

    code requires revision, since there are many things commented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,39 +6,54 @@ import { ScrollMagicPluginGsap } from "scrollmagic-plugins";
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
 // 1 smooth-scrollbar
-let scrollbar = Scrollbar.init(document.body);
+// let scrollbar = Scrollbar.init(document.body);
 
 // 2 Parallax Effect with scrollMagic
 let controller = new ScrollMagic.Controller({
   refreshInterval: 0 
 });
 
-let tweenLogo = new TimelineMax ()
-		.add([
-			TweenMax.fromTo(".firstView", 20, { yPercent: '0', ease: Power0.easeNone}, {yPercent: '80', ease: Power0.easeNone},  ),
-		]);
+    // Logo scene
+// let tweenLogo = new TimelineMax ()
+// 		.add([
+// 			TweenMax.fromTo(".firstView", 20, { yPercent: '0', ease: Power0.easeNone}, {yPercent: '50', ease: Power0.easeNone},  ),
+// 		]);
 
-let sceneLogo = new ScrollMagic.Scene({
-  triggerElement: "#video",
-  triggerHook: "onEnter",
-  duration: '100%',
-  })
-					.setTween(tweenLogo)
-					//.addIndicators() // add indicators (requires plugin)
+// let sceneLogo = new ScrollMagic.Scene({
+//   triggerElement: ".shortbio",
+//   triggerHook: "onEnter",
+//   duration: '100%',
+//   })
+// 					.setTween(tweenLogo)
+// 					//.addIndicators() // add indicators (requires plugin)
+// 					.addTo(controller)
+
+    // Bio Photo scene
+// let tweenBio = new TimelineMax ()
+// 		.add([
+// 			TweenMax.fromTo(".shortbio", 10, { yPercent: '0', ease: Power0.easeNone}, {yPercent: '100', ease: Power0.easeNone},  ),
+// 		]);
+
+let sceneBio = new ScrollMagic.Scene({
+  triggerElement: ".photo",
+  offset: '50'
+ },)
+					.setPin(".photo")
 					.addTo(controller)
 
-let tweenNumber = new TimelineMax ()
-    .add([
-      TweenMax.fromTo("#n-01", 1, {autoAlpha: 0, yPercent: -50, ease: Power0.easeNone}, {autoAlpha: 1, yPercent: 50, ease: Power0.easeNone},)
-    ])
+    // Number scene          
+// let tweenNumber = new TimelineMax ()
+//     .add([
+//       TweenMax.fromTo("#n-01", 10, {autoAlpha: 0, yPercent: -50, ease: Power0.easeNone}, {autoAlpha: 1, yPercent: 50, ease: Power0.easeNone}, 0)
+//     ])
 
-let sceneNumber = new ScrollMagic.Scene({
-  offset: 200,
-  triggerElement: ".portfolio",
-  duration: '100%',
-})
-    .setTween(tweenNumber)
-    .addTo(controller)
+// let sceneNumber = new ScrollMagic.Scene({
+//   offset: 150,
+//   triggerElement: ".portfolio",
+//   duration: '100%',
+// })
+//     .setTween(tweenNumber)
+//     .addTo(controller)
 
 // 3 Add classes to hide elements (case using JS)
 window.addEventListener("DOMContentLoaded", () => {
@@ -54,14 +69,16 @@ window.addEventListener("DOMContentLoaded", () => {
 })
 
 // 4 new scrollbar listener, instead of window.addEventListener
-scrollbar.addListener(() => {
-  console.log("Scrolling")
-  sceneLogo.refresh()
-  sceneNumber.refresh()
-    scrollFunction("PORTFOLIO", "notShown", "showUp")
-    scrollFunction("RICHI-SHOP", "notShown", "showUp")
-    scrollFunction("4PICS-1WORD", "notShown", "showUp")
-    scrollFunction("CALCULATOR", "notShown", "showUp")
-    scrollFunction("video", "videoNotShown", "showVideo")
-    scrollFunction("projects-title", "titleNotShown", "showTitle")
-  })
+// scrollbar.addListener(() => {
+//   console.log("Scrolling")
+//   // controller.update()
+//   // sceneLogo.refresh()
+//   // // sceneBio.refresh()
+//   // sceneNumber.refresh()
+//     scrollFunction("PORTFOLIO", "notShown", "showUp")
+//     scrollFunction("RICHI-SHOP", "notShown", "showUp")
+//     scrollFunction("4PICS-1WORD", "notShown", "showUp")
+//     scrollFunction("CALCULATOR", "notShown", "showUp")
+//     scrollFunction("video", "videoNotShown", "showVideo")
+//     scrollFunction("projects-title", "titleNotShown", "showTitle")
+//   })
