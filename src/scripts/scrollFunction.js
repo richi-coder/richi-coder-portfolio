c1fbefd43e50fe3d82ea0059c3347da0f6819831
richi-coder <richicoder@gmail.com>
2023-03-29 00:59:49 -0400

    blur layer working at projects and their layers with my own JS and with scrollMagic at first number 01, but not at backgroundImage photolayer

--- src/scripts/scrollFunction.js
+++ src/scripts/scrollFunction.js
@@ -1,13 +1,19 @@
 export function scrollFunction(component, notShown, showUp) {
     const projectComponent = document.getElementById(component)
-    
+    const layerComponent = document.getElementById(`l-${component}`);
         const showingComponent = projectComponent.getBoundingClientRect()
          if (showingComponent.top < window.innerHeight*0.9) {
           projectComponent.classList.remove(notShown)
           projectComponent.classList.add(showUp)
+            if(layerComponent !== null) {
+                layerComponent.classList.add("blur-layer-off")
+                }
          } else if (showingComponent.top > window.innerHeight) {
           projectComponent.classList.remove(showUp)
           projectComponent.classList.add(notShown)
+            if(layerComponent !== null) {
+                layerComponent.classList.remove("blur-layer-off")
+                }
          }
 }
 
