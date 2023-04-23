6ab28ce52ffccfa31d29d9757e504d60d8936c44
richi-coder <richicoder@gmail.com>
2023-04-23 16:02:56 -0400

    formButtons better located, buttons animated

--- src/components/FormButtons.jsx
+++ src/components/FormButtons.jsx
@@ -12,10 +12,10 @@ function FormButtons({user}) {
   const checkFulfillment = formData.formComplete === true || location === '/contact/formend' ? false : true;
 
   return (
-    <div className='h-fit text-5xl transition-all show'>
+    <div className='text-5xl transition-all show flex-grow relative'>
     {
     checkFulfillment ?
-    <div className='flex flex-row items-center justify-around w-1/2 mx-auto'>
+    <div className='flex flex-row items-center justify-around w-1/2 mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
       <FormButton direction={'backward'} />
       <FormButton direction={'forward'} user={user} />
     </div> :
--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -27,7 +27,7 @@ function SignOutButton() {
         location === '/contact' || location === '/contact/formend' ?
         <button
         onClick={logout}
-        className="bg-purple-700 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded text-2xl hover:-translate-y-[0.5rem] transition-all">
+        className="bg-purple-700 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded text-2xl hover:-translate-y-[0.5rem] transition-all ladder">
             Use another account
         </button> :
         null
