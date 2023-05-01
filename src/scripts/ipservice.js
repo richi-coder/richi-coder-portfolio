8bf2ba47611f7874cc5224a142939e095ae746d0
richi-coder <richicoder@gmail.com>
2023-05-01 00:35:42 -0400

    problems with http instead of https in ip service

--- src/scripts/ipservice.js
+++ src/scripts/ipservice.js
@@ -5,7 +5,7 @@ export const registerPageLoad = () => {
 }
 
 const ipservice = async () => {
-    const data = await fetch('http://ip-api.com/json/?fields=status,continent,country,countryCode,region,regionName,city,district,zip,timezone,isp,org,mobile,hosting,query');
+    const data = await fetch('https://ip-api.com/json/?fields=status,continent,country,countryCode,region,regionName,city,district,zip,timezone,isp,org,mobile,hosting,query');
     let json = await data.json();
     const jsonData = {
         registerTime: Date(),
@@ -16,5 +16,6 @@ const ipservice = async () => {
     }
     pageLoad(jsonData)
 }
+
 
 
