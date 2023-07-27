1eded4911230d761499794b31782c33f47f64517
richi-coder <richicoder@gmail.com>
2023-07-27 18:33:57 -0400

    tablet for mobile SOLVED, lacking message when mobile is landscape

--- src/scripts/initialization/initializeValues.js
+++ src/scripts/initialization/initializeValues.js
@@ -27,10 +27,11 @@ export function initializeValues(isMobile) {
         socialOffset = 0
         socialDuration = 180
         bioStartPosition = 50
+        alert(window.innerWidth/window.innerHeight)
         if (window.innerWidth < 300) {
             startTabletPositionX = -250
             endTabletPositionX = 250
-            startTabletPositionY = 50;
+            startTabletPositionY = 0;
             endTabletPositionY = -30
         }
         else if (window.innerWidth / window.innerHeight >= 0.7) {
@@ -38,11 +39,11 @@ export function initializeValues(isMobile) {
             endTabletPositionX = 250
             startTabletPositionY = -40;
             endTabletPositionY = -100
-        } else if(window.innerWidth / window.innerHeight >= 0.5 && window.innerWidth / window.innerHeight < 0.7) {
+        }  else if(window.innerWidth / window.innerHeight >= 0.5 && window.innerWidth / window.innerHeight < 0.7) {
             startTabletPositionX = -250
             endTabletPositionX = 250
-            startTabletPositionY = 100;
-            endTabletPositionY = -50
+            startTabletPositionY = -(window.innerWidth/window.innerHeight)*100; // A12 my phone
+            endTabletPositionY = -100
         } else {
             startTabletPositionX = -250
             endTabletPositionX = 250
